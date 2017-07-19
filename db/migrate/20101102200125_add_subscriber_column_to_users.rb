e69104c900f887dbf4cd38634a3cd3fab56498ad
mohammed <hassoun@outlook.com>
2017-07-19 18:06:46 -0400

    update migiration
    Directly inheriting from ActiveRecord::Migration is not supported in rails 5.1

--- db/migrate/20101102200125_add_subscriber_column_to_users.rb
+++ db/migrate/20101102200125_add_subscriber_column_to_users.rb
@@ -1,4 +1,4 @@
-class AddSubscriberColumnToUsers < ActiveRecord::Migration
+class AddSubscriberColumnToUsers < ActiveRecord::Migration[4.2]
   def self.up
     add_column :spree_users, :is_mail_list_subscriber, :boolean, :null => false, :default => false
   end
--- db/migrate/20101103020507_add_smc_member_id_to_users.rb
+++ db/migrate/20101103020507_add_smc_member_id_to_users.rb
@@ -1,4 +1,4 @@
-class AddSmcMemberIdToUsers < ActiveRecord::Migration
+class AddSmcMemberIdToUsers < ActiveRecord::Migration[4.2]
   def self.up
     add_column :spree_users, :mailchimp_subscriber_id, :string, :length => 31
   end
